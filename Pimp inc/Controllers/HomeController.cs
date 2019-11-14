d4d39c9493f94e68635361faadc1c9ab18ba7d58
Tyrone Kruger <37019997+tyrone0501@users.noreply.github.com>
2019-11-14 16:51:33 +0200

    Add files via upload

---
+++ Pimp inc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pimp_inc.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Our product";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Our contact page.";
+
+			return View();
+		}
+	}
+}
